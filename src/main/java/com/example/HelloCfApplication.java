384108cb8e547d6be159621163592299febe4c11
Tadashi Ito <tito@pivotal.io>
2020-03-23 15:28:32 +0900

    v0.0.1

---
+++ src/main/java/com/example/HelloCfApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// ここから追加
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+// ここまで
+
+@SpringBootApplication
+@RestController // 追加
+public class HelloCfApplication {
+
+	// ここから追加
+	@GetMapping("/") 
+	String hello() {
+			return "Hello, CF!!";
+	}
+	// ここまで
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCfApplication.class, args);
+	}
+
+}
