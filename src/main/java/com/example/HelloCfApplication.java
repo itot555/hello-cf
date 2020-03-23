fe6a170623f87da8468b20f88c35b7112250edc7
Tadashi Ito <tito@pivotal.io>
2020-03-23 23:40:02 +0900

    update

--- src/main/java/com/example/HelloCfApplication.java
+++ src/main/java/com/example/HelloCfApplication.java
@@ -2,21 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// ここから追加
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-// ここまで
 
 @SpringBootApplication
-@RestController // 追加
+@RestController
 public class HelloCfApplication {
 
-	// ここから追加
 	@GetMapping("/") 
 	String hello() {
 			return "Hello, CF v1!!";
 	}
-	// ここまで
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloCfApplication.class, args);
